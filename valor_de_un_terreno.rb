fb080633de82cf441b62563aa5a9d77d409b8c38
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 17:09:49 -0500

    Add files via upload

--- valor_de_un_terreno.rb
+++ valor_de_un_terreno.rb
@@ -1,11 +1,13 @@
 #Archivo: laboratorio1.rb
 #Autores: Mavelyn Sterling, Santiago Herrera Cabrera
+#Programas académicos: Estadística <3752> ; Ingeniería electronica <3744>
+#Profesor: Angel Garcia Baños <angel.garcia@correounivalle.edu.co>
 #Fecha creación: 2016-04-10
 #Fecha última modificación: 2016-04-17
 #Versión: 0.1
-#Licencia: GPL
+#Licencia: GPL 
 
-puts "Por favor, introduce la dimensión en metros del lado 1"
+puts "Por favor, introduce la dimensión en metros del lado 1" 
 dimension_lado1 = gets.chomp.to_f
 puts "Por favor, introduce la dimension en metros del lado 2"
 dimension_lado2 = gets.chomp.to_f
@@ -14,7 +16,7 @@ dimension_lado3 = gets.chomp.to_f
 puts "por favor, introduce el valor en pesos del metro cuadrado de terreno"
 valor_metro_cuadrado= gets.chomp.to_f
 
-dimension_lado4 = dimension_lado1 - dimension_lado3#el lado 4 es la altura del rectangulo.
+dimension_lado4 = dimension_lado1 - dimension_lado3
 area_triangulo = (dimension_lado2 * dimension_lado4) / 2 
 area_rectangulo = dimension_lado2 * dimension_lado3 
 total_terreno = (area_rectangulo *0.0001) + (area_triangulo *0.0001)
