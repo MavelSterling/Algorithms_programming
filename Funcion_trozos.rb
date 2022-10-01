f8c438eb610602dcce4b633fcadf0316426f401e
MavelSterling <32804955+MavelSterling@users.noreply.github.com>
2022-10-01 16:52:37 -0500

    Add files via upload

---
+++ Funcion_trozos.rb
@@ -0,0 +1,34 @@
+#Archivo: laboratorio2.rb
+#Autores: Mavelyn Sterling, Santiago Herrera Cabrera
+#Programas académicos: Estadística <3752> ; Ingeniería electronica <3744>
+#Profesor: Angel Garcia Baños <angel.garcia@correounivalle.edu.co>
+#Fecha creación: 2016-04-26
+#Fecha última modificación: 2016-05-01
+#Versión: 0.1
+#Licencia: GPL
+
+x = 10
+if x <= 0
+   x1 = 8 * (x * x) - 6
+  puts "f(#{x}) = #{f(x)}"
+elsif x > 0
+  x1 = (3 * (x) + 5)
+  puts "f(#{x}) = #{x1}"
+end 
+y = -7
+if y <= 0
+  y1 = (8 * (y * y)) - 6
+  puts "f(#{y}) = #{y1}"
+elsif x2 > 0
+  f = (3 * (x2) + 5)
+  puts "f(#{y}) = #{f(y1)}"
+end 
+
+z = 0
+if z <= 0
+  z1 = (8 * (z * z)) - 6
+  puts "f(#{z}) = #{z1}"
+elsif z > 0
+  z1 = (3 * (z) + 5)
+  puts "f(#{z}) = #{z1}"
+end 
